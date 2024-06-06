b18b8767e212bf70b6f11c2967874e6ae9a54db6
sergio arabaa <sergioarabaa8@gmail.com>
2024-06-07 00:12:28 +0800

    second push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// Ini Javascript
+/*ini javascript*/
+
+
+
+document.getElementById("btn-submit").addEventListener("click", function() {
+    let name = document.getElementById("name").value;
+    let birthDate = document.getElementById("birth-date").value;
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    let message = document.querySelector('textarea[name="message"]').value;
+
+    document.getElementById("sender-full-name").textContent = name;
+    document.getElementById("sender-birth-date").textContent = birthDate;
+    document.getElementById("sender-gender").textContent = gender;
+    document.getElementById("sender-full-messages").textContent = message;
+
+    // Tambahkan kode lainnya sesuai kebutuhan, seperti mengirim data ke server, dll.
+});
+
+let namebanner = prompt('Siapakah Nama Anda?',);
+document.getElementById('name-banner').innerHTML = namebanner
